7721e612976fdbcc1a6de262fef606a052ac75d7
Oceane Merlo <oceane.merlo@epita.fr>
2018-02-19 13:26:37 +0100

    new version

--- fonction.c
+++ fonction.c
@@ -3,33 +3,13 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+
+// Calcul du Delta
 int delta(int a, int b, int c)
 {
 	return ((b*b) - (4*a*c));
 }
-/*int imaginaire(int n)
-{
-	if (n%4 == 0)
-		return 1;
-	else if(n%4==1)
-		return 2;
-	else if(n%4 ==2)
-		return -1;
-	else
-		return 3;
-}
-
-char* Imaginaire(int i,char* letter)
-{
-	if(i ==2)
-	{letter[0]='i';}
-	else {if(i ==3)
-		{
-			letter[0]='-';
-			letter[1]='i';
-		}}
-	return letter;
-}*/
+//Racine carré arrondis à l'entier près
 int int_sqrt(int n) 
 {
 	int i=4, k=0,x=0,y=n;
@@ -49,54 +29,348 @@ int int_sqrt(int n)
 		return x;
 	}
 }
+//Solution Delta >0
 float* solutionP(int delta,int a, int b,float* solutions)
 {
 	double nb= delta;
 
-	float x1= (-b + int_sqrt(nb))/ (2*a);
-	float x2 =( -b - int_sqrt(nb))/(2*a);
+	float x1= (float)(-b + int_sqrt(nb))/ (float)(2*a);
+	float x2 =(float)( -b - int_sqrt(nb))/(float)(2*a);
 	solutions[0]=x1;
 	solutions[1]=x2;
 	return solutions;
 }
-int solutionS(int b, int a)
+// Solution Delta == 0
+float solutionS(int b, int a)
 {
-	int x= -b/(2*a);
+	float x= (float)(-b/(2*a));
 	return x;
 }
-/*char* solutionN(int nb, int a, int b, char* solutions)
+// Valeur absolu des solutions
+float Valeur_absolu(float nombre)
 {
-	double bis= nb;
-	int x1 = -b/(2*a);
-	int x1b = sqrt(-bis)/(2*a);
-	char prems= x1;
-	char deuz =x1b;
-	solutions[0]= prems;
-	solutions[1]= '-';
-	solutions[2]='i';
-	solutions[3]=deuz;
-	solutions[4]=';';
-	solutions[5]= prems;
-	solutions[6]= '+';
-	solutions[7]='i';
-	return solutions;
-	
-	
-}*/
+	if(nombre <0)
+		return -nombre;
+	else
+		return nombre;
+}
 
+//Forme canonique
+float canonique_carre(int a, int b)
+{
+	float c = (float)(b/(float)(2*a));
+	return c;
+}
+float canonique_delta(int a, int Delta)
+{
+	float c = (float)(Delta/(float)(4 * (a*a)));
+	return c; 
+}
+
+void Print_premier_degre(int a, int b, int c)
+{
+	
+}
+
+//Affichage de la forme canonique
+void Print_canonique(int a, int b, int Delta)
+{
+	printf("5. Nous pouvons aussi obtenir la forme canonique de ce trinome\n");
+	printf("\n");
+	printf("RAPPEL : la forme canonique de ce trinome s'écrit: ");
+	printf("   C(x) = a * [ ( x + ( b / 2 * a) )² - ( D / 4 * a² )]");
+	printf(" ou D est le delta de cette equation. \n");
+	printf("\n");
+	printf("				Ici a = %d , b = %d , D = %d\n",a,b,Delta);
+	printf("	On obient donc :\n");
+	printf("		=> C(x) = %d * [ ( x + (%d / 2 * %d) )² - ( %d / 4 * %d² )]\n",a,
+	b,a,Delta,a);
+	float premierTerme= canonique_carre(a,b);
+	float secondTerme =canonique_delta(a,Delta);
+	float prems = Valeur_absolu(premierTerme);
+	float deuz= Valeur_absolu(secondTerme);
+	if(a!=1)
+	{
+		if(premierTerme < 0 && secondTerme <0)
+		{
+			printf("			= %d * [ ( x - %.2f)² + %.2f]\n",a,prems,deuz);
+		}
+		else if(premierTerme > 0 && secondTerme >0)
+		{
+			printf("			= %d * [ ( x + %.2f)² - %.2f]\n",a,prems,deuz);
+		}
+		else if(premierTerme >0 && secondTerme <0)
+		{
+			printf("			= %d * [ ( x + %.2f)² + %.2f]\n",a,prems,deuz);
+		}
+		else
+		{
+			printf("			= %d * [ ( x - %.2f)² - %.2f]\n",a,prems,deuz);
+		}
+	}
+	else
+	{
+		if(premierTerme < 0 && secondTerme <0)
+		{
+			printf("			= ( x - %.2f)² + %.2f\n",prems,deuz);
+		}
+		else if(premierTerme > 0 && secondTerme >0)
+		{
+			printf("			= ( x + %.2f)² - %.2f\n",prems,deuz);
+		}
+		else if(premierTerme >0 && secondTerme <0)
+		{
+			printf("			= ( x + %.2f)² + %.2f\n",prems,deuz);
+		}
+		else
+		{
+			printf("			= ( x - %.2f)² - %.2f\n",prems,deuz);
+		}
+	}
+	
+}
+
+
+//Affichage des equations du second degré
+void Print_Second_degre(int a, int b, int c)
+{
+	float solP[2]={};
+	float solution;
+	float x1;
+	float x;
+	float x2;
+	//Calcul du Delta
+	int Delta= delta(a,b,c);
+	printf("Pour résoudre une equation de degre 2 voici les etapes:\n");
+	printf("1. On calcul le Delta (note D) :\n");
+	printf("			D = b² - 4 * a * c \n");
+	printf("			avec a = %d, b = %d, c = %d \n",a,b,c);
+	printf("Ce qui donne :\n");
+	printf("			D = %d² - 4*%d*%d = %d\n",b,a,c, Delta);
+	printf("2. On regarde maintenant la valeur de D pour savoir le nombres de");
+	printf(" solutions que possede l equation: \n");
+	if(Delta>0)
+		{/**************************************************************/
+			printf("Le D est positif, l equation a donc deux solutions :\n");
+			printf("3. Les solutions s ecrivent donc: \n");
+			
+			printf("			x1 = (-b + sqrt(D))/ 2*a; x2 = (-b - sqrt(D))/ 2*a \n");
+			printf("Ainsi on otient: \n"); 
+			solutionP(Delta,a,b,solP);
+			printf("			x1 = (-%d + sqrt(%d))/ 2*%d = %.2f\n",b,Delta,a,solP[0]);
+			printf(" 			x2 = (-%d - sqrt(%d))/ 2*%d = %.2f\n",b,Delta,a,solP[1]);
+			x1 = Valeur_absolu(solP[0]);
+			x2 = Valeur_absolu(solP[1]);
+			printf("RESULATS:\n");
+			printf(" 			=> D = %d \n", Delta);
+			printf("			=> x1 = %.2f \n", solP[0]);
+			printf("			=> x2 = %.2f \n", solP[1]);
+			printf("\n");
+			printf("4. Nous pouvons maintenant trouver le polynome factorise de");
+			printf(" cette equation:\n");
+			printf("RAPPEL : f(x) = a * (x - x1) * (x - x2) ou x1,x2 sont les ");
+			printf(" solutions de f(x) = 0 ( trouves dans 3. )\n");
+			printf("\n");
+			printf("Ainsi ici notre polynome factorise s'ecrit:\n");
+			if(a!=1)
+			{/**************************************************************/
+				if(solP[0]==0.00)
+				{
+					if(solP[1] < 0)
+					{
+						printf("	f(x) = a * x * ( x + x2) car x1 = 0\n");
+						printf("	Donc, f(x) = %d * x * ( x + %.2f)\n", a, x2);
+					}
+					else
+					{
+						printf("	f(x) = a * x * ( x - x2) car x1 = 0\n");
+						printf("	Donc, f(x) = %d * x * ( x - %.2f)\n", a, x2);
+					}
+				}
+				else if( solP[1]==0.00)
+				{
+					if(solP[0]<0)
+					{
+						printf("	f(x) = a* x * ( x - x1) car x2 = 0 \n");
+						printf("	Donc, f(x) = %d * x * ( x - %.2f)\n",a,x1);
+					}
+					else
+					{
+						printf("	f(x) = a* x * ( x + x1) car x2 = 0 \n");
+						printf("	Donc, f(x) = %d * x * ( x + %.2f)\n",a,x1);
+					}
+				}
+				else
+				{
+					if(solP[0]<0 && solP[1]>0)
+					{
+						printf(" f(x) = %d * x * ( x + %.2f)",a,x1);
+						printf(" * ( x - %.2f)\n",x2);
+					}
+					else if(solP[0]>0 && solP[1]<0)
+					{
+						printf(" f(x) = %d * x * ( x - %.2f)",a,x1);
+						printf(" * ( x + %.2f)\n",x2);
+					}
+					else if(solP[0] > 0 && solP[1]>0)
+					{
+						printf(" f(x) = %d * x * ( x - %.2f)",a,x1);
+						printf(" * ( x - %.2f)\n",x2);
+					}
+					else
+					{
+						printf(" f(x) = %d * x * ( x + %.2f)",a,x1);
+						printf(" * ( x + %.2f)\n",x2);
+					}
+				}
+			}
+			/**************************************************************/
+			else
+			{/**************************************************************/
+				if(solP[0]==0.00)
+				{
+					if(solP[1]>0)
+					{
+						printf("	f(x) = x * ( x - x2) car x1 = 0 et a = 1 \n");
+						printf("	Donc, f(x) = x * ( x - %.2f)\n",x2);
+					}
+					else
+					{
+						printf("	f(x) = x * ( x + x2) car x1 = 0 et a = 1 \n");
+						printf("	Donc, f(x) = x * ( x + %.2f)\n",x2);
+					}
+				}
+				else if(solP[1]==0.00)
+							{
+								if(solP[0]>0)
+								{
+									printf("	f(x) = x * ( x - x1) car x2 = 0 et a= 1 \n");
+									printf("	Donc, f(x) = x * ( x - %.2f)\n",x1);
+								}
+								else
+								{
+									printf("	f(x) = x * ( x + x1) car x2 = 0 et a= 1 \n");
+									printf("	Donc, f(x) = x * ( x + %.2f)\n",x1);
+								}					
+							}
+				else
+				{
+						if(solP[0]<0 && solP[1]>0)
+						{
+							printf(" f(x) = x * ( x + %.2f)", x1);
+							printf(" * ( x - %.2f) car a = 1 \n",x2);
+						}
+						else if(solP[0]>0 && solP[1]<0)
+						{
+							printf(" f(x) = x * ( x - %.2f)", x1);
+							printf(" * ( x + %.2f) car a = 1 \n",x2);
+						}
+						else if(solP[0] > 0 && solP[1]>0)
+						{
+							printf(" f(x) = x * ( x - %.2f)", x1);
+							printf(" * ( x - %.2f) car a = 1 \n",x2);
+						}
+						else
+						{
+							printf(" f(x) = x * ( x + %.2f)", x1);
+							printf(" * ( x + %.2f) car a = 1 \n",x2);
+						}
+					}
+					}
+					/**************************************************************/
+			
+		}
+		/**************************************************************/
+		else if(Delta==0)
+		{/**************************************************************/
+			printf("Le D est nul, l equation a donc une solution :\n");
+			printf("3. La solution s ecrit donc:\n");
+			printf("			x= a / b ;\n");
+			printf("Ainsi on obtient: \n");
+			solution = solutionS(a,b);
+			printf(" x = %d / %d = %.2f",a,b,solution);
+			x = Valeur_absolu(solution);
+			printf("RESULATS:\n");
+			printf(" 			=> D = %d \n", Delta);
+			printf("			=> x = %.2f \n", solution);
+			printf("4. Nous pouvons maintenant trouver le polynome factorise de");
+			printf(" cette equation:\n");
+			printf("RAPPEL : f(x) = a * (x - S) * (x - S) ou S est la ");
+			printf(" solution de f(x) = 0 ( trouves dans 3. )\n");
+			printf("\n");
+			printf("Ainsi ici notre polynome factorise s'ecrit:\n");
+			if(a!=1)
+			{/**************************************************************/
+				if(solution <0)
+				{
+						printf("	Donc, f(x) = %d * ( x + %.2f) * ( x + %.2f)\n", a,x,x);
+				}
+				else
+				{
+					printf("	Donc, f(x) = %d * ( x - %.2f) * ( x - %.2f)\n", a,x,x);
+				}
+		}
+		/**************************************************************/
+		else
+		{/**************************************************************/
+			if(solution <0)
+				{
+						printf("	Donc, f(x) = ( x + %.2f) * ( x + %.2f)\n",x,x);
+				}
+				else
+				{
+					printf("	Donc, f(x) = ( x - %.2f) * ( x - %.2f)\n",x,x);
+				}
+	  }/**************************************************************/
+	}
+		/**************************************************************/
+		else{/**************************************************************/
+			printf("Comme le D est negatif, comme nous sommes dans l ensemble reel");
+			printf(" il n a donc pas de solutions, nous sommes actuellement dans");
+			printf(" l ensemble des complexes\n");}
+			/**************************************************************/
+		Print_canonique(a,b, Delta);
+	}
+
+
+int char_to_int(char c)
+{
+	int n = c - '0'; 
+	return n;
+}
+
+void Print(int* str)
+{
+	while(*str)
+	{
+		printf("%d\n",*str);
+		str++;
+	}
+}
+
+int* recup_chiffres(char* data, int* recup)
+{
+	for(int i=0; i< strlen(data);i++)
+	{
+		if(data[i] >= '0' && data[i] <= '9')
+		{
+			*(recup)= char_to_int(data[i]);
+			recup++;
+		}
+	}
+	return recup;
+}	
 int main(int argc, char **argv)
 {
 	//Verification des parametres
 	if(argc<6)
 		return 0;
 	//Initialisation et attribution
-	float solP[2]={};
-	int solution;
+	int degre = atoi(argv[5]);
 	int a= atoi(argv[2]);
 	int b= atoi(argv[3]);
 	int c= atoi(argv[4]);
-	//Calcul du Delta
-	int Delta= delta(a,b,c);
+	int chiffres[3]={};
 	//INDICATION
 	printf("INDICATION:\n");
 	printf("						=> On ne cherchera les solutions de l equation");
@@ -108,39 +382,21 @@ int main(int argc, char **argv)
 	printf("RESOLUTION:\n");
 	printf("\n");
   printf("On cherche a resoudre l equation suivante: %s \n",argv[1]);
-	printf("C est une equation de degre: %d \n",atoi(argv[5]));
+	printf("C est une equation de degre: %d \n",degre);
 	printf("\n");
-	if(atoi(argv[5])== 2)
+	if(degre== 2)
+	{/**************************************************************/
+		Print_Second_degre(a,b,c);
+	}                                           
+	/**************************************************************/	
+	if(degre >3)
 	{
-		printf("Pour résoudre une equation de degre 2 voici les etapes:\n");
-		printf("1. On calcul le Delta (note D) :\n");
-		printf("			D = b² - 4 * a * c \n");
-		printf("			avec a = %d, b = %d, c = %d \n",a,b,c);
-		printf("Ce qui donne :\n");
-		printf("			D = %d² - 4*%d*%d = %d\n",b,a,c, Delta);
-		printf("2. On regarde maintenant la valeur de D pour savoir le nombres de");
-		printf(" solutions que possede l equation: \n");                                           
-		if(Delta>0){
-			printf("Le D est positif, l equation a donc deux solutions :\n");
-			printf("3. Les solutions s ecrivent donc: \n");
-			
-			printf("			x1 = (-b + sqrt(D))/ 2*a; x2 = (-b - sqrt(D))/ 2*a \n");
-			printf("Ainsi on otient: \n"); 
-			solutionP(Delta,a,b,solP);
-			printf("			x1 = (-%d + sqrt(%d))/ 2*%d = %f\n",b,Delta,a,solP[0]);
-			printf(" 			x2 = (-%d - sqrt(%d))/ 2*%d = %f\n",b,Delta,a,solP[1]);}
-		else if(Delta==0){
-			printf("Le D est nul, l equation a donc une solution :\n");
-			printf("3. La solution s ecrit donc:\n");
-			printf("			x= a / b ;\n");
-			printf("Ainsi on obtient: \n");
-			solution = solutionS(a,b);
-			printf(" x = %d / %d = %d",a,b,solution);}
-		else{
-			printf("Comme le D est negatif, comme nous sommes dans l ensemble reel");
-			printf(" il n a donc pas de solutions, nous sommes actuellement dans");
-			printf(" l ensemble des complexes\n");}
+		printf("Desole ce solveur ne prends pas les equations de degre superieur");
+		printf(" a 3. Merci de votre comprehension\n");
+		return 0;
 	}
-	
-	
+	if(degre == 1)
+	{
+		Print_Premier_degre(a,b,c);
+	}
 }
