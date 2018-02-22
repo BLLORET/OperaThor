79a7236ea0ec9f246d5da90b7107f3b4f20e2bab
Oceane Merlo <oceane.merlo@epita.fr>
2018-02-22 12:14:54 +0100

    rendu

--- operathor.c
+++ operathor.c
@@ -1,6 +1,7 @@
+
 # include "identification.h"
 # include "fonction.h"
-
+#include "troisieme.h"
 int main(int argc, char *argv[]) {
 
   if (argc < 2) errx(1 ,"Missing arguments, this program need an equation");
@@ -30,7 +31,7 @@ int main(int argc, char *argv[]) {
   printf("\n");
   printf("On cherche a resoudre l equation suivante: %s \n",argv[1]);
   printf("C est une equation de degre: %d \n",degre);
-  if(degre == 2) {/**************************************************************/
+  if(degre == 2) {/**********************************************************/
 	printf("On a :\n");
 	printf("		f(x) = %s\n",argv[1]);
 	a = cf->left[2];
@@ -43,13 +44,29 @@ int main(int argc, char *argv[]) {
   if(degre == 1) {
 	printf("Pour résoudre une equation de degre 1 voici les etapes:\n");
 	printf("On a :\n");
-    printf("		f(x) = %s\n",argv[1]);
+  printf("		f(x) = %s\n",argv[1]);
 			
 	a = cf->left[1];
 	b = cf->left[0];
 	c = cf->right[0];
 	Print_premier_degre(a,b,c);
   }
+	if(degre ==3)
+	{
+		printf("Pour résoudre une equation de degre 3 voici les etapes:\n");
+		printf("On a :\n");
+		printf("		f(x) = %s\n",argv[1]);
+		double a1 = cf->left[3];
+		double b1 = cf->left[2];
+		double c1 = cf->left[1];
+		double d1 = cf->left[0];
+		double a2 = cf->right[3];
+		double b2 = cf->right[2];
+		double c2 = cf->right[1];
+		double d2 = cf->right[0];
+		poly(a1,b1,c1,d1,a2,b2,c2,d2);
+		
+	}
 
   FreeCoeff(cf);
   return 0;
