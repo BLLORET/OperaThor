f2892a53ba654da1cc85cc13580e19f046b3bd6a
Baptiste Lloret <baptiste.lloret@epita.fr>
2018-04-23 19:48:39 +0200

    complex sans .h

--- terminal/complex.c
+++ terminal/complex.c
@@ -71,14 +71,45 @@ struct Complex *conjugue(struct Complex *a) {
 }
 
 struct Complex *div_comp(struct Complex *a, struct Complex *b) {
+  printf("                ( ");
+  Print_Complex0f(a);
+  printf(") / ( ");
+  Print_Complex0f(b);
+  printf(") = ( ");
   struct Complex *conj = conjugue(b);
+  struct Complex *res = mult_comp(a, conj);
+  Print_Complex0f(a);
+  printf(")( ");
+  Print_Complex0f(conj);
+  printf(") / (");
+  Print_Complex0f(b);
+  printf(")(");
+  Print_Complex0f(conj);
+  printf(")\n\n");
+  printf("                ( ");
+  Print_Complex0f(a);
+  printf(") / ( ");
+  Print_Complex0f(b);
+  printf(") = ( ");
+  Print_Complex0f(res);
+  printf(") / (%.0f² + ", b->real);
+  if (b->img < 0)
+    printf("%.0f²)\n\n", -b->img);
+  else
+    printf("%.0f²)\n\n", b->img);
   struct Complex *deno = mult_comp(b, conj);
   float den = deno->real;
   free(deno);
-  struct Complex *res = mult_comp(a, conj);
+  printf("                ( ");
+  Print_Complex0f(a);
+  printf(") / ( ");
+  Print_Complex0f(b);
+  printf(") = ( ");
+  Print_Complex0f(res);
+  printf(") / %.0f\n\n", den);
   free(conj);
   res->real /= den;
-  res->img /= den; 
+  res->img /= den;
   return res;
 }
 
@@ -130,26 +161,67 @@ void Soustration(struct Complex *a, struct Complex *b) {
   free(res);
 }
 
+void Print_parenth(float n) {
+  if (n < 0)
+  	printf("(%0.f)", n);
+  else
+  	printf("%0.f", n);
+}
+
+void PrintMulInt1(struct Complex *a, struct Complex *b) {
+  // a * c
+  printf("%0.f * ", a->real);
+  Print_parenth(b->real);
+  // + a * d * i
+  printf(" + (%0.f * ", a->real);
+  Print_parenth(b->img);
+  
+  // + b * c * i
+  printf(") i + (%0.f * ", a->img);
+  Print_parenth(b->real);
+  // + (b * d) i²
+  printf(") i + (%0.f * ", a->img);
+  Print_parenth(b->img);
+  printf(") i²");
+}
+
 void PrintMulInt2(struct Complex *a, struct Complex *b) {
   // a * c
   printf("%0.f * ", a->real);
-  if (b->real < 0)
-  	printf("(%0.f)", b->real);
-  else
-  	printf("%0.f", b->real);
+  Print_parenth(b->real);
   // - (b * d)
   printf(" - (%0.f * ", a->img);
-  if (b->img < 0)
-  	printf("(%0.f)", b->img);
-  else
-  	printf("%0.f", b->img);
+  Print_parenth(b->img);
   // + a * d * i
-  printf(") + ( %0.f * ", a->real);
-  if (b->img < 0)
-  	printf("(%0.f)", b->img);
-  else
-  	printf("%0.f", b->img);
+  printf(") + (%0.f * ", a->real);
+  Print_parenth(b->img);
+  // + b * c * i
+  printf(") i + (%0.f * ", a->img);
+  Print_parenth(b->real);
+  printf(") i");
+}
 
+void PrintMulInt3(struct Complex *a, struct Complex *b) {
+  // a * c
+  printf("%0.f ", a->real* b->real);
+  // - (b * d)
+  float ret = a->img * b->img;
+  if (ret < 0)
+    printf(" + %0.f ", -ret);
+  else
+  	printf("- %0.f ", ret);
+  // + a * d * i
+  ret = a->real * b->img;
+  if (ret < 0)
+    printf(" - %0.f i ", -ret);
+  else
+  	printf("+ %0.f i ", ret);
+  // + b * c * i
+  ret = a->img * b->real;
+  if (ret < 0)
+    printf(" - %0.f i", -ret);
+  else
+  	printf("+ %0.f i", ret);
 }
 
 void Multiplication(struct Complex *a, struct Complex *b) {
@@ -163,7 +235,19 @@ void Multiplication(struct Complex *a, struct Complex *b) {
   printf(") * ( ");
   Print_Complex0f(b);
   printf(") = ");
+  PrintMulInt1(a, b);
+  printf("\n\n                        Or comme i² = -1 on a:\n\n                        ( ");
+  Print_Complex0f(a);
+  printf(") * ( ");
+  Print_Complex0f(b);
+  printf(") = ");
   PrintMulInt2(a, b);
+  printf("\n\n                        ( ");
+  Print_Complex0f(a);
+  printf(") * ( ");
+  Print_Complex0f(b);
+  printf(") = ");
+  PrintMulInt3(a, b);
   printf("\n\n                        ( ");
   Print_Complex0f(a);
   printf(") * ( ");
@@ -171,6 +255,31 @@ void Multiplication(struct Complex *a, struct Complex *b) {
   struct Complex *res = mult_comp(a, b);
   printf(") = ");
   Print_Complex0f(res);
+  printf("\n\n");
+  free(a);
+  free(b);
+  free(res);
+}
+
+void Division(struct Complex *a, struct Complex *b) {
+  printf("Afin de diviser deux nombres complexes de la forme a + bi où i est\n");
+  printf("la marque de l'imaginaire, nous allons utiliser la méthode du conjugué.\n");
+  printf("Par exemble le conjugué de a + bi est a - bi. Nous allons multiplier le\n");
+  printf("numérateur par le conjugué afin d'obtenir un complexe. Le dénominateur\n");
+  printf("sera lui aussi multiplié par son conjugué afin d'obtenir un dénominateur\n");
+  printf("réel. Il ne manque plus qu'a diviser a et b par le dénominateur pour\n");
+  printf("obtenir leurs véritables valeurs.\n");
+  printf("                      Ainsi (a + bi) / (c + di) = (a + bi)(c - di) / (c + di)(c - di)\n\n");
+  printf ("Et comme multiplié un nombre complexe par son conjugué revient à additionner\n");
+  printf("les carrés de a et de b, nous avons (a + bi) * (c + di) = (a + bi)(c - di) / c² + d²\n\n\n");
+  printf("Dans notre cas nous avons \n\n");
+  struct Complex *res = div_comp(a, b);
+  printf("                ( ");
+  Print_Complex0f(a);
+  printf(") / ( ");
+  Print_Complex0f(b);
+  printf(") = ");
+  Print_Complex(res);
   printf("\n\n");
   free(a);
   free(b);
@@ -207,6 +316,7 @@ int main(int argc, char const *argv[])
   b->img = -5;
   //Addition(a, b);
   //Soustration(a, b);
-  Multiplication(a, b);
+  //Multiplication(a, b);
+  Division(a, b);
   return 0;
 }
